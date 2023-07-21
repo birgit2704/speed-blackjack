20686dec4f0418f12fd9fc3a53b16ec27bd90b61
Birgit <birgit.bartenschlager@web.de>
2023-07-21 09:41:39 +0200

    html and css structure

--- index.js
+++ index.js
@@ -1,11 +1,34 @@
 const startBtn = document.getElementById("start-btn");
+const level1Btn = document.getElementById("level1-btn");
+const level2Btn = document.getElementById("level2-btn");
+const level3Btn = document.getElementById("level3-btn");
+const playerStartBtn = document.getElementById("player-start-btn");
+const anotherCardBtn = document.getElementById("another-card-btn");
+const giveUpBtn = document.getElementById("give-up-btn");
+const showDealerCardsBtn = document.getElementById("show-dealer-card-btn");
+const level1El = document.getElementById("level1");
+const level2El = document.getElementById("level2");
+const level3El = document.getElementById("level3");
+const gameAreaEl = document.getElementById("game-area");
+const rulesEl = document.getElementById("rules");
+const timerEl = document.getElementById("timer");
 
-startBtn.addEventListener("click", handleStartGame);
+startBtn.addEventListener("click", startGame);
+level1Btn.addEventListener("click", startLevel);
 
-function handleStartGame() {
+function startGame() {
   document.getElementById("header").style.display = "none";
   startBtn.style.display = "none";
+  level1El.style.display = "block";
 }
+
+function startLevel() {
+  level1El.style.display = "none";
+  gameAreaEl.style.display = "flex";
+  timerEl.style.display = "block";
+}
+
+// rules and timer elements
 
 // text effect typewriter
 document.addEventListener("DOMContentLoaded", typeWriter);
