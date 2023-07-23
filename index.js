74c0557f20965d24ca6adbd331386f95b33f757a
Birgit <birgit.bartenschlager@web.de>
2023-07-23 13:36:19 +0200

    responsiveness cars and text

--- index.js
+++ index.js
@@ -1,28 +1,34 @@
 // play with two decks
+//delay next level message
 
 const startBtn = document.getElementById("start-btn");
+
 const level1Btn = document.getElementById("level1-btn");
 const level2Btn = document.getElementById("level2-btn");
 const level3Btn = document.getElementById("level3-btn");
+
 const playerStartBtn = document.getElementById("player-start-btn");
 const newCardBtn = document.getElementById("new-card-btn");
 const noNewCardBtn = document.getElementById("no-new-card-btn");
+
 const level1El = document.getElementById("level1");
 const level2El = document.getElementById("level2");
 const level3El = document.getElementById("level3");
 const gameAreaEl = document.getElementById("game-area");
 const timerEl = document.getElementById("timer");
+
 let deckId = "";
 let dealerScore = 0;
 let playerScore = 0;
 let cardsHtmlDealerComplete = "";
 let winningGames = 0;
+let ofGames;
 let inLevel1 = true;
 let inLevel2 = false;
 let inLevel3 = false;
 let earlyEnd = false;
 let timer;
-let remainingCardsInDeck;
+let remainingCardsInDeck = 52;
 let deductAce1 = 0;
 let deductAce2 = 0;
 let deductAce3 = 0;
@@ -42,21 +48,6 @@ const replace = {
   ACE: "11",
 };
 
-function checkGame() {
-  if (inLevel1 && winningGames > 1) {
-    earlyEnd = true;
-    continueGame();
-  }
-  if (inLevel2 && winningGames > 2) {
-    earlyEnd = true;
-    continueGame();
-  }
-  if (inLevel3 && winningGames > 3) {
-    earlyEnd = true;
-    continueGame();
-  }
-}
-
 document.getElementById("rules").addEventListener("click", () => {
   document.getElementById("rules-modal").style.display = "block";
 });
@@ -64,26 +55,15 @@ document.getElementById("close-rules-btn").addEventListener("click", () => {
   document.getElementById("rules-modal").style.display = "none";
 });
 startBtn.addEventListener("click", startBlackjack);
-level1Btn.addEventListener("click", startLevel);
 playerStartBtn.addEventListener("click", startGame);
 noNewCardBtn.addEventListener("click", endGame);
 newCardBtn.addEventListener("click", () => {
   getCards(1, "player");
+  remainingCardsInDeck -= 1;
 });
-
+level1Btn.addEventListener("click", startLevel);
 level2Btn.addEventListener("click", startLevel);
 level3Btn.addEventListener("click", startLevel);
-
-function startGame() {
-  playerStartBtn.disabled = true;
-  playerScore = 0;
-  dealerScore = 0;
-  deductAce1 = 0;
-  deductAce2 = 0;
-  deductAce3 = 0;
-  getCards(2, "dealer");
-  getCards(2, "player");
-}
 
 function startBlackjack() {
   document.getElementById("header").style.display = "none";
@@ -92,6 +72,15 @@ function startBlackjack() {
 }
 
 function startLevel() {
+  if (inLevel2) {
+    document.getElementById("level").textContent = "Level 2";
+  }
+  if (inLevel3) {
+    document.getElementById("level").textContent = "Level 3";
+  }
+  winningGames = 0;
+  ofGames = inLevel1 ? 4 : inLevel2 ? 5 : 6;
+  remainingCardsInDeck = 52;
   level1El.style.display = "none";
   level2El.style.display = "none";
   level3El.style.display = "none";
@@ -104,22 +93,22 @@ function startLevel() {
     "player-cards"
   ).innerHTML = `<div class="placeholder"></div>
   <div class="placeholder"></div>`;
+
   playerStartBtn.disabled = false;
   startAndEndTimer();
+  updateRemainingCards();
 }
 
-function startAndEndTimer() {
-  timerEl.style.display = "block";
-  winningGames = 0;
-  document.getElementById("score").textContent = `Games won: ${winningGames}`;
-  getDeck();
-  if (inLevel1) startCountdown();
-  else if (inLevel2 || (inLevel3 && earlyEnd === true)) {
-    setTimeout(function () {
-      earlyEnd = false;
-      startCountdown();
-    }, 500);
-  }
+function startGame() {
+  remainingCardsInDeck -= 4;
+  playerStartBtn.disabled = true;
+  playerScore = 0;
+  dealerScore = 0;
+  deductAce1 = 0;
+  deductAce2 = 0;
+  deductAce3 = 0;
+  getCards(2, "dealer");
+  getCards(2, "player");
 }
 
 function getDeck() {
@@ -131,6 +120,16 @@ function getDeck() {
       console.log(data);
       console.log(remainingCardsInDeck);
     });
+}
+
+function updateRemainingCards() {
+  document.getElementById(
+    "cards-remaining"
+  ).textContent = `Cards remaining: ${remainingCardsInDeck}`;
+  if (remainingCardsInDeck < 1) {
+    displayResultMessage("no more cards left", 2000);
+    setTimeout(continueNextLevel, 2000);
+  }
 }
 
 function getCards(num, player) {
@@ -160,12 +159,15 @@ function getCards(num, player) {
     `;
           })
           .join("");
+
+        // Treat ACE either as 1 or 11
         if (num === 1 && data.cards[0].value === "ACE") {
           deductAce3 = 10;
           console.log("deduct3", deductAce3);
         }
       }
       renderCards(num, player, cardsHtml, data);
+      updateRemainingCards();
     });
 }
 
@@ -194,17 +196,12 @@ function determineWinner(player, data, num) {
   }
 
   // treat ACE either as 1 or as 11
-
   if (player === "player" && num === 2 && data.cards[0].value === "ACE") {
     deductAce1 = 10;
   }
   if (player === "player" && num === 2 && data.cards[1].value === "ACE") {
     deductAce2 = 10;
   }
-
-  // if (num === 1 && data.cards[0].value === "ACE") {
-  //   deductAce3 = 10;
-  // }
 
   if (playerScore > 21 && (deductAce1 || deductAce2 || deductAce3)) {
     if (deductAce1) {
@@ -264,7 +261,6 @@ function endGame() {
       displayResultMessage("it's a tie");
     }
   }
-
   document.getElementById("dealer-cards").innerHTML = cardsHtmlDealerComplete;
   newCardBtn.disabled = true;
   noNewCardBtn.disabled = true;
@@ -275,60 +271,74 @@ function endGame() {
 
 function renderWinningGames() {
   winningGames += 1;
-  document.getElementById("score").textContent = `Games won: ${winningGames}`;
+
+  document.getElementById(
+    "score"
+  ).textContent = `Games won: ${winningGames} of ${ofGames}`;
   checkGame();
 }
-// rules and timer elements
-function startCountdown() {
-  let count = 60;
-  let timer = setInterval(function () {
-    document.getElementById("countdown").innerHTML = count--;
-    if (earlyEnd) clearInterval(timer);
-    if (count === -1) {
-      clearInterval(timer);
-      newCardBtn.disabled = true;
-      noNewCardBtn.disabled = true;
-      playerStartBtn.disabled = true;
-      displayResultMessage("time is up", 4000);
-      continueGame();
-    }
-  }, 1000);
+
+function checkGame() {
+  if (inLevel1 && winningGames === 4) {
+    playerStartBtn.disabled === true;
+    earlyEnd = true;
+    continueNextLevel();
+  }
+  if (inLevel2 && winningGames === 5) {
+    playerStartBtn.disabled === true;
+    earlyEnd = true;
+    continueNextLevel();
+  }
+  if (inLevel3 && winningGames === 6) {
+    playerStartBtn.disabled === true;
+    earlyEnd = true;
+    continueNextLevel();
+  }
 }
 
-function continueGame() {
+function continueNextLevel() {
   if (inLevel1) {
-    if (winningGames > 1) {
+    if (winningGames > 3) {
       inLevel1 = false;
       inLevel2 = true;
-      displayResultMessage("you reached level 2", 4000);
-      level2El.style.display = "block";
-      gameAreaEl.style.display = "none";
-    } else {
       setTimeout(function () {
+        displayResultMessage("you reached level 2", 4000);
+        level2El.style.display = "block";
+        gameAreaEl.style.display = "none";
+      }, 2000);
+    } else {
+      setTimeout(() => {
         window.location.reload();
       }, 4000);
     }
   } else if (inLevel2) {
-    if (winningGames > 2) {
+    if (winningGames > 4) {
       inLevel2 = false;
       inLevel3 = true;
-      displayResultMessage("you reached the final level", 4000);
-      level3El.style.display = "block";
-      gameAreaEl.style.display = "none";
-    } else {
       setTimeout(function () {
+        displayResultMessage("you reached the final level", 4000);
+        level3El.style.display = "block";
+        gameAreaEl.style.display = "none";
+      }, 2000);
+    } else {
+      setTimeout(() => {
         window.location.reload();
-      }, 4000);
+      }, 3000);
     }
   } else if (inLevel3) {
-    if (winningGames > 3) {
+    if (winningGames > 6) {
       inLevel3 = false;
-      displayResultMessage("Congratulations, you won the game", 6000);
       setTimeout(function () {
-        window.location.reload();
-      }, 6000);
+        displayResultMessage("Congratulations, you won the game", 6000);
+        playerStartBtn.style.display = "none";
+        newCardBtn.style.display = "none";
+        noNewCardBtn.style.display = "none";
+        setTimeout(() => {
+          window.location.reload();
+        }, 6000);
+      }, 2000);
     } else {
-      setTimeout(function () {
+      setTimeout(() => {
         window.location.reload();
       }, 4000);
     }
@@ -338,12 +348,43 @@ function continueGame() {
 function displayResultMessage(text, time = 1000) {
   document.getElementById("result-message").textContent = `${text}`;
   document.getElementById("result-message").style.display = "inline";
-  setTimeout(function () {
+  setTimeout(() => {
     document.getElementById("result-message").style.display = "none";
   }, time);
 }
 
-// text effect typewriter
+function startAndEndTimer() {
+  timerEl.style.display = "block";
+
+  document.getElementById("score").textContent = `Games won: 0 of 4`;
+
+  getDeck();
+  if (inLevel1) startCountdown();
+  else if (inLevel2 || (inLevel3 && earlyEnd === true)) {
+    setTimeout(() => {
+      earlyEnd = false;
+      startCountdown();
+    }, 500);
+  }
+}
+
+function startCountdown() {
+  let count = 60;
+  let timer = setInterval(() => {
+    document.getElementById("countdown").innerHTML = count--;
+    if (earlyEnd) clearInterval(timer);
+    if (count === -1) {
+      clearInterval(timer);
+      newCardBtn.disabled = true;
+      noNewCardBtn.disabled = true;
+      playerStartBtn.disabled = true;
+      displayResultMessage("time is up", 4000);
+      continueNextLevel();
+    }
+  }, 1000);
+}
+
+// text effect typewriter on initial page
 document.addEventListener("DOMContentLoaded", typeWriter);
 
 let i = 0;
